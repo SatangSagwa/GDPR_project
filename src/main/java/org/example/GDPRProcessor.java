2c5d34fbb7abe014bd2b1a6eb1b04be5253f86c8
SatangSagwa <emilia.ivarsson@live.se>
2024-11-18 10:09:31 +0100

    added anonymize method

--- src/main/java/org/example/GDPRProcessor.java
+++ src/main/java/org/example/GDPRProcessor.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class GDPRProcessor {
+    public void anonymizeUser(User user) {
+        user.setName(null);
+        user.setEmail(null);
+        user.setPassword(null);
+
+        if(user.getName() == null &&
+                user.getEmail() == null &&
+                user.getPassword() == null) {
+            System.out.println("User data has been removed.");
+        } else {
+            System.out.println("Failed to remove data.");
+        }
+    }
     // i den här klassen ska ni implementera dessa metoder:
     // 1. anonymizeUser() - metod för att anonymisera en användare
     // 2. logDataProcessing() - metod som loggar varje gång en användare behandlas
+    public void logDataProcessing() {
+
+    }
 }
